828975bf00c7343b3520b53ed5bcdd6eafba70bf
mathewmarcus <mathewmarcus456@gmail.com>
2017-07-21 01:06:18 -0500

    created WifiPortal destructor and replaced sscanf with strtok to prevent buffer overflows

--- src/portal.c
+++ src/portal.c
@@ -1,43 +1,68 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "portal.h"
 
-#define LINE_LENGTH 104 // handle '\0' return from fgets and three '\t' delimeters
-
 WifiPortal* newWifiPortal(int line_length) {
+  
   WifiPortal* wifi_portal = malloc(sizeof(WifiPortal));
   if (!wifi_portal) {
     fprintf(stderr, "WifiPortalError: failed to allocate memory for WifiPortal");
     return NULL;
   }
 
-  max_conn_string_len = line_length - sizeof(wifi_portal->ip_address) - sizeof(wifi_portal->ssid);
-  
-  wifi_portal->auth_string = (char *) malloc(sizeof(char) * max_conn_string_len);
+  wifi_portal->ssid[SSID_LEN] = '\0';
+  wifi_portal->protocol[PROTOCOL_LEN] = '\0';
+  wifi_portal->ip_address[IP_ADDRESS_LEN] = '\0';  
+
+  /* wifi_portal->auth_string = (char *) malloc(sizeof(char) * AUTH_STRING_LEN); */
   if (!wifi_portal->auth_string) {
     fprintf(stderr, "WifiPortalError: failed to allocate memory for WifiPortal auth string");
     free((void *) wifi_portal);
     return NULL;
   }
 
-  return wifi_portal
+  return wifi_portal;
 }
 
-WifiPortal* parse_config(File* config, char* ssid) {
-  WifiPortal* wifi_portal = (WifiPortal *) malloc(sizeof(WifiPortal));
+void delWifiPortal(WifiPortal* wifi_portal) {
+  if (wifi_portal != NULL) {
+    free(wifi_portal->auth_string);
+    free(wifi_portal);
+  }
+}
 
-  char* format_string = "%s\t%s\t%s\t%s\t"
+WifiPortal* parse_config(char* config_path, char* ssid) {
+  FILE* config = fopen(config_path, "r");
+  if (config == NULL) {
+    fprintf(stderr, "WifiPortalError: Failed to read config file: %s\n", config_path);
+    return NULL;
+  }
+  
+  WifiPortal* wifi_portal = newWifiPortal(LINE_LENGTH);
+
+  char* format_string = "%s\t%s\t%s\t%s";
   char line[LINE_LENGTH];
 
-  while (fgets(line, LINE_LENGTH, config)) {
-    sscanf(line,
-	   format_string,
-	   &(wifi_portal->ssid),
-	   &(wifi_portal->protocol),
-	   &(wifi_portal->ip_address),
-	   &(wifi_portal->auth_string));
-    if (strncmp(wifi_portal->ssid, ssid, 32))
-      return wifi_portal;
+  while (fgets(line, LINE_LENGTH, config) != NULL) {
+    char* tmp = strtok(line, "\t");
+
+    strncpy(wifi_portal->ssid, tmp, SSID_LEN);
+    if (strncmp(wifi_portal->ssid, ssid, SSID_LEN) != 0)
+      continue;
+    
+    tmp = strtok(NULL, "\t");
+    strncpy(wifi_portal->protocol, tmp, PROTOCOL_LEN);
+
+    tmp = strtok(NULL, "\t");
+    strncpy(wifi_portal->ip_address, tmp, IP_ADDRESS_LEN);
+    
+    tmp = strtok(NULL, "\0");
+    printf("%s\n", tmp);
+    wifi_portal->auth_string= strdup(tmp);
+    
+    return wifi_portal;
   }
+  delWifiPortal(wifi_portal);
   return NULL;
 }
--- src/portal.h
+++ src/portal.h
@@ -1,17 +1,20 @@
 #ifndef PORTAL_H
 #define PORTAL_H
 
-#include <stdio.h>
+#define LINE_LENGTH 104
+#define SSID_LEN 32
+#define IP_ADDRESS_LEN 15
+#define PROTOCOL_LEN 5
 
 typedef struct wifi_portal {
-  char ssid[32];
-  char ip_address[15];
-  char protocol[5];
+  char ssid[SSID_LEN + 1];
+  char ip_address[IP_ADDRESS_LEN + 1];
+  char protocol[PROTOCOL_LEN + 1];
   char* auth_string;
 } WifiPortal;
 
 WifiPortal* newWifiPortal(int line_length);
 void delWifiPortal(WifiPortal* wifi_portal);
-WifiPortal* parse_config(File* config);
+WifiPortal* parse_config(char* config_path, char* ssid);
 
 #endif
